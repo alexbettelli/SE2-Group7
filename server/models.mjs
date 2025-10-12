a93043d46ef928226e0ef3cfd806a6dec0d58f77
zudiay <zdaytac@gmail.com>
2025-10-12 22:24:43 +0200

    add Counter model

--- server/models.mjs
+++ server/models.mjs
@@ -5,4 +5,15 @@ function Service(id, name, tag, average_time) {
   this.average_time = average_time;
 }
 
-export { Service }
+// TODO should we allow multiple services per counter?
+
+function Counter(id, number, service_id, service_tag, service_name, is_busy) {
+  this.id = id;
+  this.number = number;
+  this.service_id = service_id;
+  this.service_tag = service_tag;
+  this.service_name = service_name;
+  this.is_busy = is_busy || false;
+}
+
+export { Service, Counter }
