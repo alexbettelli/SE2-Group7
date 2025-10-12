8b70178a4a776aac36244b69f78b22f9d7c40c7e
zudiay <zdaytac@gmail.com>
2025-10-12 23:50:50 +0200

    add counter to client model

--- client/src/models.mjs
+++ client/src/models.mjs
@@ -5,4 +5,13 @@ function Service(id, name, tag, average_time) {
   this.average_time = average_time;
 }
 
-export { Service }
+function Counter(id, number, service_id, service_tag, service_name, is_busy) {
+  this.id = id;
+  this.number = number;
+  this.service_id = service_id;
+  this.service_tag = service_tag;
+  this.service_name = service_name;
+  this.is_busy = is_busy || false;
+}
+
+export { Service, Counter }
