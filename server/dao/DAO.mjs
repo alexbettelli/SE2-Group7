ac40f545ddb972548de9db46253d78e615de3174
ALEX BETTELLI s338059 <s338059@studenti.polito.it>
2025-10-13 18:33:21 +0200

    Implementation of queue  and getNextTicket()

--- server/dao/DAO.mjs
+++ server/dao/DAO.mjs
@@ -1,12 +1,31 @@
 import sqlite from 'sqlite3'
+import dayjs from 'dayjs';
 import {Service, Counter} from '../models/models.mjs'
 
 const db = new sqlite.Database('./database.db', (err) => {
     if(err) throw err;
 })
 
+//TICKET
+const closeTicket = (ticket_Id) => {
+  return new Promise((resolve, reject) => {
+    const now = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    const query = `
+        UPDATE Ticket
+        SET finalDate = ?
+        WHERE id = ?;
+    `;
+    db.run(query, [now, ticket_Id], function(err) {
+        if (err) return reject(err);
+        // this.changes = number of modified lines. If 0 no changes!
+        if (this.changes === 0) return reject(new Error('Ticket not found'));
+        resolve(`Ticket ${ticket_Id} chiuso correttamente`);
+    });
+  });
+}
+
 //SERVICES
- const getAllServices = () => {
+const getAllServices = () => {
   return new Promise((resolve, reject) => {
     const query = 'SELECT * FROM Service;'
     db.all(query, (err, rows) => {
@@ -21,9 +40,21 @@ const db = new sqlite.Database('./database.db', (err) => {
     })
   });
 }
+//extract the list of services assignet to the specific counter_id
+const getServicesAssignedToCounter = (counter_id) => {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT service_id
+                       FROM ServiceAssignment
+                       WHERE counter_id = ?`
 
+        db.all(query,[counter_id] , (err, rows) => {
+            if(err) reject(err);
+            const serviceIDs = rows.map(r => r.service_id);
+            resolve(serviceIDs)
+        })
+    })
+}
 // COUNTERS
-
 // TODO implement busy status based on current tickets
 const getAllCounters = () => {
   return new Promise((resolve, reject) => {
@@ -47,5 +78,5 @@ const getAllCounters = () => {
   });
 }
 
-const DAO = {getAllServices, getAllCounters}
+const DAO = {getAllServices, getServicesAssignedToCounter, getAllCounters, closeTicket}
 export default DAO;
--- server/models/models.mjs
+++ server/models/models.mjs
@@ -13,7 +13,67 @@ function Counter(id, number, service_id, service_tag, service_name, is_busy) {
   this.service_name = service_name;
   this.is_busy = is_busy || false;
 }
+function Queues(){
+    this.queues = new Map;
 
+    this.inizializeQueues = (servicesInfo) =>{ //servicesInfo: array of { serviceID, avgServiceTime }
+        this.queues.clear();
 
+        servicesInfo.forEach(service => {
+            this.queues.set(
+                service.serviceID, 
+                {
+                    avgServiceTime: service.avgServiceTime,
+                    tickets: []
+                }
+            );
+        });
+    }
 
-export { Service, Counter }
+    this.addTicket = (serviceID, customerID, ticketID) => {
+        const serviceQueue = this.queues.get(serviceID);
+        if (!serviceQueue) throw new Error(`Service ${serviceID} not found`);
+        serviceQueue.tickets.push({ customerID, ticketID });
+        console.log(this.queues);
+    }
+
+    this.getNextTicket = (serviceIDs)=> {
+        let selectedService = null;
+        let maxLength = -1;
+
+        for (const serviceID of serviceIDs) {
+            const serviceQueue = this.queues.get(serviceID);
+            if (!serviceQueue) continue;
+
+            const len = serviceQueue.tickets.length;
+
+            if (len > maxLength) {
+                selectedService = serviceID;
+                maxLength = len;
+            } else if (len === maxLength && len > 0) {// 2 queues have the same length
+                // compute avgServiceTime and return the fastest
+                if (serviceQueue.avgServiceTime < this.queues.get(selectedService).avgServiceTime) {
+                    selectedService = serviceID;
+                }
+            }
+        }
+
+        if (!selectedService || maxLength === 0) return null;
+
+        return this.queues.get(selectedService).tickets.shift();// {customerID, ticketID}
+    }
+    this.removeTicket = (customerID) => {
+        if (!customerID) return;
+
+        this.queues.forEach(serviceQueue => {
+            serviceQueue.tickets = serviceQueue.tickets.filter(
+                ticket => ticket.customerID !== customerID
+            );
+        });
+
+        console.log(`Removed tickets for socket ${customerID}`);
+        console.log(this.queues);
+    }
+}
+
+export { Service, Counter, Queues }
