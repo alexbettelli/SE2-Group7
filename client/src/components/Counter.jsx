b7accf2637b38e89b244500d20a573140a9ea52d
SAMUELE SCAPELLATO s337235 <s337235@studenti.polito.it>
2025-10-14 19:40:03 +0200

    fixed

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -31,46 +31,9 @@ function Counter({ counter }) {
   };
 
   return (
-    <>
-      { 
-        //counter ? 
-          <Display counter={counter} ticket={ticket} handleNextTicket={handleNextTicket}/>
-         /*: (<div className="counter-details">
-            <h2 className="counter-details-title">Select a Counter</h2>
-            <p className="no-counter-selected">No counter selected. Please select a counter to view details.</p>
-          </div>)*/
-      }
-    </>
+    <Display counter={counter} ticket={ticket} handleNextTicket={handleNextTicket}/>
   )
 
-  /*
-  return (
-    <div className="counter-details">
-      <h2 className="counter-details-title">Counter Details</h2>
-      <p className="counter-details-info">Counter Number: {counter.number}</p>
-      <p className="counter-details-info">Service Name: {counter.service_name}</p>
-      <p className="counter-details-info">Service Tag: {counter.service_tag}</p>
-
-      {ticket ? (
-        <div className="ticket-container">
-          <div className="ticket-card">
-            <h3>Ticket #  {ticket.number}{ticket.serviceTag}</h3>
-            <p><span className="label">ID:</span> {ticket.id}</p>
-            <p><span className="label">Service Tag:</span> {ticket.serviceTag}</p>
-            <p><span className="label">Counter Number:</span> {ticket.counterNumber}</p>
-            <p><span className="label">Initial:</span> {ticket.initialDate}</p>
-          </div>
-        </div>
-      ) : (
-        <p className="no-ticket"> Current Ticket: None</p>
-      )}
-      <button
-        onClick={handleNextTicket}
-      >
-        Next Ticket
-      </button>
-    </div>
-  );*/
 }
 
 function Display(props){
