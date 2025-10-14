96b09372ad6944b2bce2f118084c3b584a1c0373
zudiay <zdaytac@gmail.com>
2025-10-14 23:15:46 +0200

    remove counter select from Employee.jsx

--- client/src/pages/Employee.jsx
+++ client/src/pages/Employee.jsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
-import { getCounters } from '../API/API.mjs';
-import { CounterList } from '../components/CounterList';
-import { Counter } from '../components/Counter';
+import React, {useEffect, useState} from 'react';
+import {getCounters} from '../API/API.mjs';
+import {CounterList} from '../components/CounterList';
 import '../style/Employee.css';
-
 
 
 function EmployeePage() {
@@ -28,9 +25,8 @@ function EmployeePage() {
   return (
     <div className='employee-container'>
       <CounterList counters={counters} setSelectedCounter={handleCounterSelect} />
-      <Counter counter={selectedCounter} />
     </div>
-  
+
   );
 }
 
